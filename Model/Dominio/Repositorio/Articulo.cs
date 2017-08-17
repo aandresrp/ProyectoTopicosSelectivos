ef55503aca20a36e9cf553623b63e7f384f60b95
aandresrp <andresulatina@gmail.com>
2017-08-17 11:33:28 -0600

    Proyecto
    Selectos

---
+++ Model/Dominio/Repositorio/Articulo.cs
@@ -0,0 +1,64 @@
+﻿//using ProyectoSelectos.GenericRepository;
+using Proyectos.Ulatina.Model;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Ulatina.Topicos.GenericRepository;
+
+namespace Proyecto.Ulatina.BL.Dominio.Repositorio
+{
+    internal class Articulo
+    {
+        
+        private ProyectoFinalEntities _context = new ProyectoFinalEntities();
+        //private DbContext elcontecto = new DbContext();
+            
+
+
+        public Articulo()
+        {
+        }
+
+        internal IList<Proyectos.Ulatina.Model.Articulo> ListarTodos()
+        {
+            IList<Proyectos.Ulatina.Model.Articulo> resultado = _context.Articulo.ToList();
+                return resultado;
+        }
+
+        internal void agregarArticulo(string nombre, string color, string descripcion)
+        {
+            Proyectos.Ulatina.Model.Articulo articuloNuevo = new Proyectos.Ulatina.Model.Articulo();
+            articuloNuevo.nombre = nombre;
+            articuloNuevo.color = color;
+            articuloNuevo.descripcion = descripcion;
+
+            _context.Articulo.Add(articuloNuevo);
+            _context.SaveChanges();
+
+        }
+
+
+        internal Proyectos.Ulatina.Model.Articulo ConsultaProductosPorNumero(int miNumeroDeArticulo)
+        {
+            Proyectos.Ulatina.Model.Articulo elArticulo = _context.Articulo.Find(miNumeroDeArticulo);
+            return elArticulo;
+        }
+
+        internal IList<Proyectos.Ulatina.Model.Articulo> ConsultaArticuloPorNombre(string nombreDelProducto)
+        {
+            IList<Proyectos.Ulatina.Model.Articulo> elArticulo = _context.Articulo.Where<Proyectos.Ulatina.Model.Articulo>(p => p.nombre.Contains(nombreDelProducto)).ToList();
+            return elArticulo;
+        }
+
+        internal void borrarArticulo(int numeroArticuloABorrar)
+        {
+            Proyectos.Ulatina.Model.Articulo articulo = new Articulo()._context.Articulo.Find(numeroArticuloABorrar);
+
+            _context.Articulo.Remove(articulo);
+            _context.SaveChanges();
+
+        }
+
+
+    }
+}
---
+++ Model/Dominio/Repositorio/Permiso.cs
@@ -0,0 +1,39 @@
+﻿using Proyectos.Ulatina.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dominio.Repositorio
+{
+    internal class Permiso
+    {
+        private ProyectoFinalEntities _context = new ProyectoFinalEntities();
+        //private DbContext elcontecto = new DbContext();
+
+
+
+        public Permiso()
+        {
+        }
+
+        internal IList<Proyectos.Ulatina.Model.Permiso> ListarTodos()
+        {
+            IList<Proyectos.Ulatina.Model.Permiso> resultado = _context.Permiso.ToList();
+            return resultado;
+        }
+
+        internal void crearRegistro(Proyectos.Ulatina.Model.Permiso permiso)
+        {
+            _context.Permiso.Add(permiso);
+        }
+
+        internal void eliminarRegistro(int idDelaPersona, string Permiso)
+        {
+            _context.Permiso.Where();
+        }
+
+
+    }
+}
